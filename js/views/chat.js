abdf739bfaddbc0f81ad91eccecaac10425b45ab
SuyuBoy <helloworld.suyu@gmail.com>
2026-05-30 17:38:39 +0800

    fix: simpleMarkdown 支持 > blockquote 引用渲染

--- js/views/chat.js
+++ js/views/chat.js
@@ -85,6 +85,7 @@ function simpleMarkdown(text) {
     .replace(/^### (.+)$/gm, "<h3>$1</h3>")
     .replace(/^## (.+)$/gm, "<h2>$1</h2>")
     .replace(/^# (.+)$/gm, "<h1>$1</h1>")
+    .replace(/^&gt; (.+)$/gm, "<blockquote>$1</blockquote>")
     .replace(/\*\*(.+?)\*\*/g, "<strong>$1</strong>")
     .replace(/\*(.+?)\*/g, "<em>$1</em>")
     .replace(/`([^`]+)`/g, "<code>$1</code>")
